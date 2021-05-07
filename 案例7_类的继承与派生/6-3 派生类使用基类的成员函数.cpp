823371ef873da206597b1da30af08483bddc0934
SWQXDBA2 <SWQXDBA2>
2021-05-07 18:02:23 +0800

    1

--- 案例7_类的继承与派生/6-3 派生类使用基类的成员函数.cpp
+++ 案例7_类的继承与派生/6-3 派生类使用基类的成员函数.cpp
@@ -1,4 +1,30 @@
-//
-// Created by SWQXDBA2 on 2021/5/7.
-//
+#include <iostream>
+#include <cstdio>
+#include <string>
 
+using namespace std;
+class Animal{
+private:
+    int m_nWeightBase;
+protected:
+    int m_nAgeBase;
+public:
+    void set_weight(int w){
+        m_nWeightBase=w;
+    }
+    void set_age(int a){
+        m_nAgeBase=a;
+    }
+    int get_weight(){
+        return m_nWeightBase;
+    }
+};
+class Cat: public Animal{
+private:
+    string m_strName;
+public:
+    Cat(const string &mStrName) : m_strName(mStrName) {}
+    void   print_age(){
+        printf("%s, age = %d\n",m_strName.c_str(),m_nAgeBase);
+    }
+};
