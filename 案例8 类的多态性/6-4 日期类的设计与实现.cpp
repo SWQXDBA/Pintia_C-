a1a4e08f13ac206be3f372165d0dcbb49040e08e
SWQXDBA2 <SWQXDBA2>
2021-05-09 23:31:56 +0800

    1

--- 案例8 类的多态性/6-4 日期类的设计与实现.cpp
+++ 案例8 类的多态性/6-4 日期类的设计与实现.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 ///////////////////////提交以下代码段////////////////////////
- const string months [] = {nullptr,"January","February","March","April","May","June","July","August","September"
-                                 ,"October","November","December"};
+
+
 class MyDate{
 private:
-    int days [] = {-1,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days [13]{};
     int month;
     int day;
     int year;
+    //是否是闰年
     static bool  isRun(int year){
         if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ){
             return true;
@@ -19,22 +20,49 @@ private:
         return false;
     }
 public:
-    MyDate(int month, int day, int year) : month(month), day(day), year(year) {}
+    void setDate(int newM, int newD, int newY) {
+        //检测是否合法
+        if(newM<=0||newD<=0||newM>12){
+            month=1,day=1,year=1900;
+            return;
+        }
+        //检测天是否合法
+        int date[] = {-1,31,28,31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(isRun(newY))
+            date[2]=29;
+        if(day>date[newM]){
+            month=1,day=1,year=1900;
+            return;
+        }
+        month=newM,day=newD,year=newY;
 
-    void setDate(int i, int i1, int i2) {
-        month=i,day=i1,year=i2;
+
+        if(isRun(newY))
+            days[2]=29;
+
     }
-
     MyDate() {
         month=1,day=1,year=1900;
+        int date[] = {-1,31,28,31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int i=0;i<13;i++){
+            days[i]=date[i];
+        }
+        if(isRun(year))
+            days[2]=29;
     }
-
     friend ostream &operator<<(ostream &os, const MyDate &date) {
-        os << months[date.month] <<" "<< " day: " << date.day << ", " << date.year;
+        //用于输出月份名称
+        const string months [] = {"nullptr","January","February","March","April","May","June","July","August","September"
+                ,"October","November","December"};
+        os << months[date.month] <<" "<< date.day << ", " << date.year;
         return os;
     }
-    friend MyDate&  operator++(MyDate &date,int ){
+    friend MyDate operator++(MyDate &date,int ){
         MyDate temp(date);
+        ++date;
+        return temp;
+    }
+    friend MyDate& operator++(MyDate &date){
         if(isRun(date.year))
             date.days[2]=29;else{
             date.days[2]=28;
@@ -53,48 +81,13 @@ public:
             date.days[2]=29;else{
             date.days[2]=28;
         }
-        return temp;
+        return date;
     }
-     friend MyDate& operator++(MyDate &date){
-         if(isRun(date.year))
-             date.days[2]=29;else{
-             date.days[2]=28;
-             }
-        date.day++;
-             if(date.day>date.days[date.month]){
-                 date.month++;
-                 date.day=1;
-             }
-             if(date.month>12){
-                 date.year++;
-                 date.month=1;
-             }
 
-         if(isRun(date.year))
-             date.days[2]=29;else{
-             date.days[2]=28;
-         }
-         return date;
-    }
     friend MyDate operator+=(MyDate &date,int d){
-        if(isRun(date.year))
-            date.days[2]=29;else{
-            date.days[2]=28;
-        }
-        date.day+=d;
-        if(date.day>date.days[date.month]){
-            date.month+;
-            date.day=1;
-        }
-        if(date.month>12){
-            date.year++;
-            date.month=1;
-        }
-
-        if(isRun(date.year))
-            date.days[2]=29;else{
-            date.days[2]=28;
-        }
+        for(int i=0;i<d;i++)
+            date++;
+        return date;
     }
 };
 
@@ -103,11 +96,13 @@ public:
 ///////////////////////提交以上代码段////////////////////////
 int main()
 {
+
     int m,d,y;
     MyDate d1,d2,d3;
     cin>>m>>d>>y;
     d1.setDate(m,d,y);
 
+    cout<<(d1+=7);
     cin>>m>>d>>y;
     d2.setDate(m,d,y);
 
@@ -124,4 +119,5 @@ int main()
     cout << "\n\nTesting the postfix increment operator:\n"<< " d3 is " << d3 << endl;
     cout << "d3++ is " << d3++ << endl;
     cout << " d3 is " << d3 <<endl;
-} 
+
+}
